bfe40dcc5fc94cfffbec06cbf204bdce87e87904
Kelly <kellyjeangraham@gmail.com>
2017-01-25 19:34:04 -0800

    starting lego family project

---
+++ lego-bojack-family/sketch.js
@@ -0,0 +1,26 @@
+// VARIABLES
+var leftStart = 10;
+var rightStart = 1190;
+
+function setup() {
+  createCanvas(1200, 720);
+  background(255, 245, 255);
+  noStroke();
+}
+
+function draw() {
+  
+}
+
+// CHARACTERS
+
+function bojack() {}
+
+function todd() {}
+
+function princessCarolyn() {}
+
+function diane() {}
+
+function mrPeanutButter() {}
+
