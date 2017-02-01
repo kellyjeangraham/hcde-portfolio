20945a07a9b9449a5247f0f5c9f9897e23887298
Kelly <kellyjeangraham@gmail.com>
2017-01-31 19:26:31 -0800

    starting to work on interactive monster assignment

---
+++ monster/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(1200, 720); // draw the working canvas
+}
+
+function draw() {
+  background(245, 230, 245); // light pink background in draw to keep it refreshing
+}
+
+function monster(a, b) { // don't forget to include some parameters, this is the larger monster function that will call smaller body part functions
+  
+}
