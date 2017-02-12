fa0afcf8aabd8db8152029c8ac6aa74e2fe13f77
Kelly <kellyjeangraham@gmail.com>
2017-02-11 19:09:58 -0800

    cleaned up and commented

--- color-picker/sketch.js
+++ color-picker/sketch.js
@@ -1,25 +1,24 @@
 /* 
   Created by Kelly Graham
   Last edited 2/11/17
-  "Bloom" click to draw a circle of the color of the image behind it
+  "Bloom" click to draw a circle of the color of the image behind it, slowly abstract a photograph
 */
 
-function preload() {
-  bg = loadImage("rainbowflower.jpg");
+function preload() { // preload an image
+  bg = loadImage("rainbowflower.jpg"); // put image in variable to call when loaded
 }
 
 function setup() {
-  createCanvas(720, 720);
-  noStroke();
-  image(bg, 0, 0, 720, 720);
+  createCanvas(720, 720); // draw square canvas
+  noStroke(); // remove strokes
+  image(bg, 0, 0, 720, 720); // draw image to fill canvas
 }
 
 function draw() {
- if (mouseIsPressed) {
-   var c = get(mouseX, mouseY);
-  var f = color(c, 100);
-  fill(f);
-  ellipse(mouseX, mouseY, 50, 50);
+ if (mouseIsPressed) { // check if mouse is clicked or held down
+  var c = get(mouseX, mouseY); // fetch color values for position when mouse was held down
+  fill(c); // fill with stored color values
+  ellipse(mouseX, mouseY, 40, 40); // draw circle at mouse location with image-grabbed color
   }
 }
 
