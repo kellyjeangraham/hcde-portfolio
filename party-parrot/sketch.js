17f3889ccef54a1f822113fb08717f6376f7b7b9
Kelly <kellyjeangraham@gmail.com>
2017-02-11 19:10:17 -0800

    cleaned up and commented

--- party-parrot/sketch.js
+++ party-parrot/sketch.js
@@ -5,30 +5,26 @@
   Library from: https://github.com/antiboredom/p5.gif.js
 */
 
-var parrot;
+var parrot; // set up gif variable
 
 function setup() {
-  createCanvas(800, 600);
-  parrot = loadGif('partyparrot.gif');
+  createCanvas(800, 600); // draw canvas
+  parrot = loadGif('partyparrot.gif'); // use P5.gif.js library to import partyparrot gif
 }
 
 function draw() {
-  background(245, 200, 200);
-  fill(255, 245, 245);
-  textSize(72);
-  textAlign(CENTER);
-  text("press p to party", width/2, height/2);
+  background(245, 200, 200); // draws background each draw() cycle
+  fill(255, 245, 245); // fill color for text
+  textSize(72); // set text size
+  textAlign(CENTER); // align text to center of page
+  text("press p to party", width/2, height/2); // draw guide text
   
-  if (key == "p") {
-    background(255, 220, 220);
-    for (i = 0; i < 35; i++) {
-      for (j = 0; j < 25; j++) {
-        image(parrot, i*35, j*25);
+  if (key == "p") { // check if p key is currently pressed
+    background(255, 220, 220); // redraw background on press
+    for (i = 0; i < 35; i++) { // for loop to draw rows of gifs
+      for (j = 0; j < 25; j++) { // nested loop to draw columns of gifs
+        image(parrot, i*35, j*25); // draw gifs along the for loop iterations
       }
     }
   }
-}
-
-function keyTyped() {
-  
 }
